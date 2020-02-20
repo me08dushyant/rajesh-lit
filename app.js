dd06af267ad8f53bdb6a2d0bd4f9ab06908acc4b
Rajesh Shrivastava <shrivastavarajesh1990@gmail.com>
2020-02-20 14:09:22 +0530

    update
    update

--- app.js
+++ app.js
@@ -1,8 +1,59 @@
 import { html, render } from 'https://unpkg.com/lit-html/lit-html.js';
 
-let app = () => html`
-<h1>some html goes here Yes</h1>
-<h2> hi therer hi </h2>
+let headertop = () => html`
+<div class="headbar-top">
+	<div class="grid-container">
+	  <div class="grid-x grid-margin-x">
+	   <div class="cell auto align-self-middle ">
+	     <span >  +91 9319 466 656 | +91 9717 303 287 | +91 93194 66656 </span>
+	  </div> 
+	   <div class="cell shrink">
+	     <div class="grid-x  ">
+		 <div class="cell shrink"> <i class="step fi-social-facebook size-24"></i>  <i class="step fi-mail size-24"></i> <i class="step fi-social-youtube size-24"></i> <i class="step fi-social-flickr size-24"></i>  </div>	
+		 </div>
+	  </div> 
+	  </div>
+	</div>  
 `;
+render(headertop(),document.getElementById('header-top'));
 
-render(app(),document.getElementById('header'));
+let navbar = () => html`
+<div class="navbar-top">
+	<div class="grid-container">
+		  <div class="grid-x grid-margin-x">
+			<div class=" cell auto align-self-middle">
+			logo
+			</div>
+			<div class="cell shrink">
+					<ul class="dropdown menu" data-dropdown-menu>
+					  <li><a href="index.html">Home</a></li>
+					  <li><a href="about-us.html">About Us </a></li>	
+					  <li><a href="#">Lead Performer</a></li>				  
+					 <li>
+						<a href="#">Shows</a>
+						<ul class="menu">
+						  <li class="size-18"><a href="#">Corporate Shows</a></li>
+						  <li class="size-18" ><a href="#">Celebrities Shows</a></li>
+						  <li class="size-18" ><a href="#">Wedding Events</a></li>
+						  <!-- ... -->
+						</ul>
+					  </li>
+					 <li>
+						<a href="#">Cleints</a>
+						<ul class="menu">
+						  <li class="size-18" ><a href="#">International</a></li>
+						  <li class="size-18" ><a href="#">Indian</a></li>
+						 
+						  <!-- ... -->
+						</ul>
+					  </li>
+					   <li><a href="media.html">Media</a></li>
+					  <li><a href="contact.html">Contact Us</a></li>
+					</ul>
+			</div>
+		  </div>
+		 
+	</div>
+	</div>
+`;
+render(navbar(),document.getElementById('nav-bar'));
---
+++ app1.js
@@ -0,0 +1,98 @@
+import { html, render } from 'https://unpkg.com/lit-html/lit-html.js';
+
+let headertop = () => html`
+<div class="headbar-top">
+	<div class="grid-container">
+	  <div class="grid-x grid-margin-x">
+	   <div class="cell auto align-self-middle ">
+	     <span >  +91 9319 466 656 | +91 9717 303 287 | +91 93194 66656 </span>
+	  </div> 
+	   <div class="cell shrink">
+	     <div class="grid-x  ">
+		 <div class="cell shrink"> <i class="step fi-social-facebook size-24"></i>  <i class="step fi-mail size-24"></i> <i class="step fi-social-youtube size-24"></i> <i class="step fi-social-flickr size-24"></i>  </div>	
+		 </div>
+	  </div> 
+	  </div>
+	</div>  
+`;
+render(headertop(),document.getElementById('header-top'));
+
+let navbar = () => html`
+<div class="navbar-top">
+	<div class="grid-container">
+		  <div class="grid-x grid-margin-x">
+			<div class=" cell auto align-self-middle">
+			logo
+			</div>
+			<div class="cell shrink">
+					<ul class="dropdown menu" data-dropdown-menu>
+					  <li><a href="index.html">Home</a></li>
+					  <li><a href="about-us.html">About Us </a></li>	
+					  <li><a href="#">Lead Performer</a></li>				  
+					 <li>
+						<a href="#">Shows</a>
+						<ul class="menu">
+						  <li class="size-18"><a href="#">Corporate Shows</a></li>
+						  <li class="size-18" ><a href="#">Celebrities Shows</a></li>
+						  <li class="size-18" ><a href="#">Wedding Events</a></li>
+						  <!-- ... -->
+						</ul>
+					  </li>
+					 <li>
+						<a href="#">Cleints</a>
+						<ul class="menu">
+						  <li class="size-18" ><a href="#">International</a></li>
+						  <li class="size-18" ><a href="#">Indian</a></li>
+						 
+						  <!-- ... -->
+						</ul>
+					  </li>
+					   <li><a href="media.html">Media</a></li>
+					  <li><a href="contact.html">Contact Us</a></li>
+					</ul>
+			</div>
+		  </div>
+		 
+	</div>
+	</div>
+`;
+render(navbar(),document.getElementById('nav-bar'));
+
+
+let footer = () => html`
+<div class="footer">
+		<div class="grid-container">
+			<div class="grid-x grid-margin-x">
+				<div class="cell large-6 ">
+				<h5> About Us </h5>
+				<p> Zeal Fire Dance Company is one of the best dance troupe in Agra. All dancers are professionally trained and lots of experience of events,celebrity shows. About choreographer-Anil Verma.He has 17 years experience of all types of dances.He is a D.I.D Fame.His troupe performed with lots of celebrities like BABA SEHGAL,MAMTA SHARMA (FAME-ANARKALI DISCO CHALI), Kavita KrishnaMurti, Vinod Rathore, Ananya Idol, Harshi Mad and many more </p>
+				</div>
+				<div class="cell large-6 ">
+				<h5> Contact Us </h5>
+				<strong>Download Presentation PDF </strong> <br/>
+	
+				Lets put stage on fire. Feel free to contact ZFDC - zealfiredancecompany <br/>
+	
+				Phone:    9837273741, 9717303287 <br/>
+				Whatsapp: 9837273741 <br/>
+				Email: zfdc07@gmail.com 
+				zeal.fire.troupe@gmail.com <br/>
+				Address: B-108 1st Floor Central Bank Road Opp Bank of India Kamla Nagar Agra 
+				</div>
+	
+			</div>
+			
+		</div>
+		
+	<div class="logo-placeholder">
+	<p class="text-center">
+	
+	<a href="#"  class="hollow button" >Download Presentation PDF </a>
+	</p>
+	</div>	
+	
+	</div>
+	
+`;
+
+render(footer(),document.getElementById('footer'));
